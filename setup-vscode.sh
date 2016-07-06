ea466ec67f6078d18c5734cfb3c4fe7c2be9cb6b
Toru Kobayashi <koba0004@gmail.com>
2016-07-07 00:52:26 +0900

    Add settings.json of VSCode

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -1,3 +1,12 @@
 #!/bin/sh -xe
-ln -s "${HOME}/repos/github/dotfiles/.vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
-ln -s "${HOME}/repos/github/dotfiles/.vscode/keybindings.json" "${HOME}/Library/Application Support/Code - Insiders/User/keybindings.json"
+
+SRC_DIR="${HOME}/repos/github/dotfiles/.vscode/"
+DEST_CODE_DIR="${HOME}/Library/Application Support/Code/User/"
+DEST_CODE_INSIDER_DIR="${HOME}/Library/Application Support/Code - Insiders/User/"
+
+ln -s "${SRC_DIR}keybindings.json" "${DEST_CODE_DIR}keybindings.json"
+ln -s "${SRC_DIR}keybindings.json" "${DEST_CODE_INSIDER_DIR}keybindings.json"
+
+ln -s "${SRC_DIR}settings.json" "${DEST_CODE_DIR}settings.json"
+ln -s "${SRC_DIR}settings.json" "${DEST_CODE_INSIDER_DIR}settings.json"
+
