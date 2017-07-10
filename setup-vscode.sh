7c1d1e8d2a5f9a00a53da3a828a50cd372429e90
Toru Kobayashi <koba0004@gmail.com>
2017-07-11 01:38:45 +0900

    Remove conde-insiders from README

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -xe
 
-SRC_DIR="${HOME}/repos/github/dotfiles/.vscode/"
+SRC_DIR="${HOME}/repos/github.com/koba04/dotfiles/.vscode/"
 DEST_CODE_DIR="${HOME}/Library/Application Support/Code/User/"
-DEST_CODE_INSIDER_DIR="${HOME}/Library/Application Support/Code - Insiders/User/"
 
 ln -s "${SRC_DIR}keybindings.json" "${DEST_CODE_DIR}keybindings.json"
-ln -s "${SRC_DIR}keybindings.json" "${DEST_CODE_INSIDER_DIR}keybindings.json"
-
 ln -s "${SRC_DIR}settings.json" "${DEST_CODE_DIR}settings.json"
-ln -s "${SRC_DIR}settings.json" "${DEST_CODE_INSIDER_DIR}settings.json"
 
